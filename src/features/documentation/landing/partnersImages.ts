bfae119140c04925608fe2d0d517f8995abd1cb5
christian harrington <christian@bulkbit.systems>
2025-03-02 20:14:13 +0000

    add company section

---
+++ src/features/documentation/landing/partnersImages.ts
@@ -0,0 +1,74 @@
+import { Image } from 'react-grid-gallery';
+
+export const companyImages: Image[] = [
+  {
+    src: '/companies/8vc.png',
+    width: 100,
+    height: 100,
+    tags: [{ value: 'Investor', title: 'Investor' }],
+    caption: 'Pre-Seed Investor',
+  },
+  {
+    src: '/companies/BalancerV3.png',
+    width: 250,
+    height: 100,
+    tags: [{ value: 'Partner', title: 'Infrastructure' }],
+    caption: 'Launch Partner',
+  },
+  {
+    src: '/companies/Mako.png',
+    width: 120,
+    height: 100,
+    tags: [{ value: 'Investor', title: 'Investor' }],
+    caption: 'Pre-Seed Investor',
+  },
+  {
+    src: '/companies/chainlink_build.png',
+    width: 190,
+    height: 110,
+    tags: [{ value: 'Partner', title: 'Chainlink BUILD program member' }],
+    caption: 'Industry leading data and automation',
+  },
+  {
+    src: '/companies/longhashx.png',
+    width: 120,
+    height: 100,
+    tags: [{ value: 'RVR', title: 'Rebalancing efficiency' }],
+    caption: 'LVR rebalancing efficiency',
+  },
+  {
+    src: '/companies/Hypernest.png',
+    width: 100,
+    height: 100,
+    tags: [{ value: 'Accelerator', title: 'Cohort 1' }],
+    caption: 'Pre-Seed',
+  },
+  {
+    src: '/companies/Codehawks.png',
+    width: 150,
+    height: 100,
+    tags: [{ value: 'Competitive Audits', title: 'Auditor' }],
+    caption: 'OP backed and sponsored',
+  },
+  {
+    src: '/companies/Cyfrin.png',
+    width: 150,
+    height: 100,
+    tags: [{ value: 'Auditor', title: 'Private Audit' }],
+    caption: 'Industry leading private audits',
+  },
+  {
+    src: '/companies/Marshland.png',
+    width: 100,
+    height: 100,
+    tags: [{ value: 'Investor', title: 'Investor' }],
+    caption: 'Pre-Seed Investor',
+  },
+  {
+    src: '/companies/369.png',
+    width: 100,
+    height: 100,
+    tags: [{ value: 'Investor', title: 'Investor' }],
+    caption: 'Pre-Seed Investor',
+  },
+];
