bea2828e87e6bb39079b22eecec5e735546080d6
christian harrington <christian@bulkbit.systems>
2025-03-11 22:08:50 +0000

    reformat document

--- src/features/documentation/landing/landingPage.tsx
+++ src/features/documentation/landing/landingPage.tsx
@@ -1,18 +1,14 @@
-import { Grid } from "antd";
-import { LandingPageMobile } from "./landingPageMobile";
-import LandingPageDesktop from "./landingPageDesktop";
+import { Grid } from 'antd';
+import { LandingPageMobile } from './landingPageMobile';
+import LandingPageDesktop from './landingPageDesktop';
 
 const { useBreakpoint } = Grid;
 
 export function LandingPage() {
+  const screens = useBreakpoint();
+  const isMobile = !screens.lg && !screens.xl && !screens.xxl;
 
-
-    const screens = useBreakpoint();
-    const isMobile = !screens.lg && !screens.xl && !screens.xxl;
-
-    return (
-            isMobile ? <LandingPageMobile /> : <LandingPageDesktop />
-    );
+  return isMobile ? <LandingPageMobile /> : <LandingPageDesktop />;
 }
 
 export default LandingPage;
