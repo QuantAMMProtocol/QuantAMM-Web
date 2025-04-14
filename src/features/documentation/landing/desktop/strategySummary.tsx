a6f0e2a5ccdc4a484c5ee987837b6c16bf9ac89e
bulkcade <christian@bulkbit.systems>
2025-04-14 23:09:33 +0100

    rework strategy explainer section

--- src/features/documentation/landing/desktop/strategySummary.tsx
+++ src/features/documentation/landing/desktop/strategySummary.tsx
@@ -1,6 +1,5 @@
-import { Button, Carousel, Col, Row, Typography } from 'antd';
+import { Button, Col, Row, Typography } from 'antd';
 import { ProductItemBackground } from '../../../productExplorer/productItem/productItemBackground';
-import { ROUTES } from '../../../../routesEnum';
 import { useEffect, useState } from 'react';
 import { SimulationRunBreakdown } from '../../../simulationResults/simulationResultSummaryModels';
 import { getBreakdown, Pool } from '../../../../services/breakdownService';
@@ -12,19 +11,7 @@ const { Title } = Typography;
 export function StrategySummary() {
   const [breakdowns, setBreakdowns] = useState<SimulationRunBreakdown[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [visibleCard, setVisibleCard] = useState<number>(0);
-
-  const handleCarouselChange = (current: number) => {
-    setVisibleCard(current);
-  };
-
-  const carouselRuleKeys = [
-    'Momentum',
-    'AntiMomentum',
-    'Channel Following',
-    'Power Channel',
-    'Power Channel',
-  ];
+  const [strategy, setStrategy] = useState<string>("Momentum");
 
   // Effect to load breakdowns whenever the tab key changes
   useEffect(() => {
@@ -83,13 +70,13 @@ export function StrategySummary() {
             </Col>
           </Row>
           <Row>
-            <Col span={1}></Col>
-            <Col span={6}>
+            <Col span={7}>
               <div
                 style={{
                   justifyContent: 'center',
                   alignItems: 'center',
-                  paddingTop: '15vh',
+                  paddingTop: '40px',
+                  marginLeft:'20px'
                 }}
               >
                 <h4 style={{ textAlign: 'center', margin: 0 }}>
@@ -98,176 +85,177 @@ export function StrategySummary() {
                 <p style={{ textAlign: 'center', margin: 0 }}>
                   FULLY DECENTRALISED, FULLY TRANSPARENT
                 </p>
-                <p style={{ textAlign: 'center', margin: 0 }}>
-                  DO NOT TRUST OPAQUE MANAGERS
-                </p>
-                <Carousel
-                  arrows={true}
-                  autoplay={true}
-                  autoplaySpeed={3000}
-                  afterChange={handleCarouselChange}
-                >
-                  <div>
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        paddingBottom: '20px',
-                      }}
-                    >
+                <Row gutter={[8,8]} style={{ marginTop: '2vh', padding: 0 }}>
+                  <Col span={24} style={{ margin: 0, padding: 0 }}>
+                  <Row>
+                    <Col span={8}>
+                    <div style={{justifyContent:'center', alignItems:'center'}}>
                       <img
                         loading="lazy"
                         style={{
-                          width: '40%',
-                          height: '40%',
+                          width: '100%',
+                          height: 'auto',
                           padding: '15px',
                         }}
                         src="/documentation/vanilla_momentum.svg"
                       />
-                      <h4 style={{ textAlign: 'center', marginBottom: 0 }}>
-                        Trend Following
-                      </h4>
-                      <p style={{ textAlign: 'center', marginTop: 0 }}>
-                        It&apos;s easier to buy high and sell higher than to buy
-                        low and sell high
+                      </div>
+                    </Col>
+                    <Col span={13}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                      <Col span={24}><h4 style={{ margin: 0, padding: 0 }}>Momentum</h4></Col>
+                      
+                      <Col span={24}><p style={{ paddingRight:'10px' }}>
+                      It&apos;s hard to buy low and sell high. It&apos;s easier to buy high and sell higher. Follow the trend.
                       </p>
-                      <Button
-                        style={{ marginBottom: '10px' }}
+                      </Col>
+                      </div>
+                    </Col>
+                    <Col span={3}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                        <Button
                         type="primary"
                         size="small"
                         onClick={() =>
-                          (window.location.href = `${ROUTES.DOCUMENTATION}/SimpleMomentum`)
+                          setStrategy('Momentum')
                         }
                       >
-                        View Momentum Documentation
+                      Apply
                       </Button>
-                    </div>
-                  </div>
-                  <div>
-                    <div
+                      </div>
+                      </Col>
+                  </Row>
+                  </Col>
+                  <Col span={24} style={{ margin: 0, padding: 0 }}>
+                  <Row>
+                    <Col span={8}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                    <img
+                      loading="lazy"
                       style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        paddingBottom: '20px',
+                        width: '100%',
+                        height: 'auto',
+                        padding: '15px',
                       }}
-                    >
-                      <img
-                        loading="lazy"
-                        style={{
-                          width: '50%',
-                          height: '50%',
-                        }}
-                        src="/documentation/mean_reversion.svg"
+                      src="/documentation/mean_reversion.svg"
                       />
-                      <h4 style={{ textAlign: 'center', marginBottom: 0 }}>
-                        Price Mean Reversion
-                      </h4>
-                      <p style={{ textAlign: 'center', marginTop: 0 }}>
-                        deviations will revert back to the mean. Buy and sell
-                        assuming prices will revert
+                      </div>
+                    </Col>
+                    <Col span={13}>
+                      <Col span={24}><h4 style={{ margin: 0, padding: 0 }}>Price Mean Reversion</h4></Col>
+                      
+                      <Col span={24}><p style={{ paddingRight:'10px' }}>
+                      Deviations will revert back to the mean. Buy and sell assuming prices will revert
                       </p>
-
+                      </Col>
+                    </Col>
+                  <Col span={3}>
+                  <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
                       <Button
-                        style={{ marginBottom: '10px' }}
-                        type="primary"
-                        size="small"
-                        onClick={() =>
-                          (window.location.href = `${ROUTES.DOCUMENTATION}/AntiMomentum`)
-                        }
-                      >
-                        View Mean Reversion Documentation
-                      </Button>
+                      type="primary"
+                      size="small"
+                      onClick={() =>
+                      setStrategy('AntiMomentum')
+                    }
+                    >
+                      Apply
+                    </Button>
                     </div>
-                  </div>
-                  <div>
-                    <div
+                    </Col>
+                  </Row>
+                  </Col>
+                  <Col span={24} style={{ margin: 0, padding: 0 }}>
+                  <Row>
+                    <Col span={8}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                    <img
+                      loading="lazy"
                       style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        paddingBottom: '20px',
+                        width: '100%',
+                        height: 'auto',
+                        padding: '15px',
                       }}
-                    >
-                      <img
-                        loading="lazy"
-                        style={{
-                          width: '50%',
-                          height: '50%',
-                        }}
-                        src="/documentation/channel_following.svg"
+                      src="/documentation/channel_following.svg"
                       />
-                      <h4 style={{ textAlign: 'center', marginBottom: 0 }}>
-                        Channel Following
-                      </h4>
-                      <p style={{ textAlign: 'center', marginTop: 0 }}>
-                        Capitalize on small movements but act fast on larger
-                        movements
-                      </p>
-                      <Button
-                        style={{ marginBottom: '10px' }}
-                        type="primary"
-                        size="small"
-                        onClick={() =>
-                          (window.location.href = `${ROUTES.DOCUMENTATION}/ChannelFollowing`)
-                        }
-                      >
-                        View Channel Following Documentation
-                      </Button>
+                      </div>
+                    </Col>
+                    <Col span={13}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                      <Col span={24}><h4 style={{ margin: 0, padding: 0 }}>Channel Following</h4></Col>
+                      
+                      <Col span={24}><p style={{ paddingRight:'10px' }}>
+                      Everything will revert to the mean on small movements but act fast on larger movements
+                      </p></Col>
+                      </div>
+                    </Col>
+                    <Col span={3}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                    <Button
+                      type="primary"
+                      size="small"
+                      onClick={() =>
+                      setStrategy('Channel Following')
+                      }
+                    >
+                      Apply
+                    </Button>
                     </div>
-                  </div>
-                  <div>
-                    <div
+                    </Col>
+                  </Row>
+                  </Col>
+                  
+                  <Col span={24} style={{ margin: 0, padding: 0 }}>
+                  <Row>
+                    <Col span={8}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                    <img
+                      loading="lazy"
                       style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        paddingBottom: '20px',
+                        width: '100%',
+                        height: 'auto',
+                        padding: '15px',
                       }}
-                    >
-                      <img
-                        loading="lazy"
-                        style={{
-                          width: '50%',
-                          height: '50%',
-                          padding: '15px',
-                        }}
-                        src="/documentation/power_channel.svg"
+                      src="/documentation/power_channel.svg"
                       />
-                      <h4 style={{ textAlign: 'center', marginBottom: 0 }}>
-                        Power Channel
-                      </h4>
-                      <p style={{ textAlign: 'center', marginTop: 0 }}>
-                        Ignore the noise of small price movements, act fast on
-                        large price movements
-                      </p>
+                      </div>
+                    </Col>
+                    <Col span={13}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
+                      <Col span={24}><h4 style={{ margin: 0, padding: 0 }}>Power Channel</h4></Col>
+                      <Col span={24}><p style={{ paddingRight:'10px' }}>
+                      Ignore the noise of small price movements, act fast on large price movements
+                      </p></Col>
+                      </div>
+                    </Col>
+                    <Col span={3}>
+                    <div style={{justifyContent:'center', alignContent:'center', height:'100%'}}>
                       <Button
-                        style={{ marginBottom: '10px' }}
-                        type="primary"
-                        size="small"
-                        onClick={() =>
-                          (window.location.href = `${ROUTES.DOCUMENTATION}/PowerChannel`)
-                        }
-                      >
-                        View Power Channel Documentation
-                      </Button>
+                      type="primary"
+                      size="small"
+                      onClick={() =>                        
+                          setStrategy('Power Channel')
+                      }
+                    >
+                      Apply
+                    </Button>
                     </div>
-                  </div>
-                </Carousel>
-              </div>
+                    </Col>
+                  </Row>
+                  </Col>
+                </Row>
+                </div>
             </Col>
             <Col span={8}>
               <Row>
                 <Col span={24} style={{ paddingTop: '40px' }}>
-                  <h4 style={{ textAlign: 'center', margin: 0 }}>
+                  <h4 style={{ textAlign: 'center', margin: 0,paddingLeft: '50px', paddingRight: '30px' }}>
                     Traditional DEX Pool Holdings
                   </h4>
-                  <p style={{ textAlign: 'center', margin: 0 }}>
+                  <p style={{ textAlign: 'center', margin: 0,paddingLeft: '50px', paddingRight: '30px' }}>
                     Focus on earning fees, ignore price movements
                   </p>
                   <div
-                    style={{ paddingLeft: '80px', paddingRight: '80px' }}
+                    style={{ paddingLeft: '70px', paddingRight: '30px', marginTop:'2vh' }}
                     hidden={
                       (loading && breakdowns.length == 0) ||
                       breakdowns.filter(
@@ -291,15 +279,14 @@ export function StrategySummary() {
                   </div>
                 </Col>
                 <Col span={24} style={{ paddingBottom: '40px' }}>
-                  <h4 style={{ textAlign: 'center', margin: 0 }}>
+                  <h4 style={{ textAlign: 'center', margin: 0,paddingLeft: '50px', paddingRight: '30px' }}>
                     QuantAMM BTF Pool Holdings
                   </h4>
-                  <p style={{ textAlign: 'center', margin: 0 }}>
+                  <p style={{ textAlign: 'center', margin: 0,paddingLeft: '50px',paddingRight: '30px' }}>
                     React to markets while earning fees.
                   </p>
-                  <p style={{ textAlign: 'center', margin: 0 }}></p>
                   <div
-                    style={{ paddingLeft: '80px', paddingRight: '80px' }}
+                    style={{ paddingLeft: '70px', paddingRight: '30px' }}
                     hidden={
                       (loading && breakdowns.length == 0) ||
                       breakdowns.filter(
@@ -314,7 +301,7 @@ export function StrategySummary() {
                         breakdowns.filter(
                           (x) =>
                             x.simulationRun.updateRule.updateRuleName ==
-                              carouselRuleKeys[visibleCard]
+                              strategy
                         )[0]
                       }
                       overrideChartTheme="ag-default-dark"
@@ -339,7 +326,7 @@ export function StrategySummary() {
                   breakdowns={breakdowns.filter(
                     (x) =>
                       x.simulationRun.updateRule.updateRuleName ==
-                        carouselRuleKeys[visibleCard] ||
+                    strategy ||
                       x.simulationRun.updateRule.updateRuleSimKey == 'hodl'
                   )}
                   forceViewResults={true}
