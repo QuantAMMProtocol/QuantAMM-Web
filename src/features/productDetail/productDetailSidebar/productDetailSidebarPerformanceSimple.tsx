8b2f7e5a315f3e777330001b638582941fadfaf4
christian harrington <christian@bulkbit.systems>
2026-01-02 11:03:03 +0000

    simplified crypto native view progress

---
+++ src/features/productDetail/productDetailSidebar/productDetailSidebarPerformanceSimple.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { PerformancePeriod, Product } from '../../../models';
+import { ProductItemPerformanceAreaGraph } from '../../productExplorer/productItem/card/productItemPerformanceAreaGraph';
+
+import styles from './productDetailSidebarGraph.module.scss';
+
+interface ProductDetailSidebarOverviewGraphProps {
+  product: Product;
+}
+
+export const ProductDetailSidebarPerformanceSimple: FC<
+  ProductDetailSidebarOverviewGraphProps
+> = ({ product }) => {
+  const performanceLength = (product.dailyPerformance?.length ?? 0);
+  const performancePeriod:PerformancePeriod = {sharePrice: 0, return: 0};
+  return (
+    <div className={styles['product-detail-sidebar-graph']}>
+      {product.oneWeekPerformance &&
+        product.oneMonthPerformance &&
+        product.threeMonthPerformance &&
+        product.sixMonthPerformance &&
+        product.oneYearPerformance &&
+        product.inceptionPerformance && (
+          <ProductItemPerformanceAreaGraph
+            data={[
+              product.inceptionPerformance,
+              performanceLength >= 7 ? product.oneWeekPerformance : performancePeriod,
+              performanceLength >= 30 ? product.oneMonthPerformance : performancePeriod,
+              performanceLength >= 90 ? product.threeMonthPerformance : performancePeriod,
+              performanceLength >= 180 ? product.sixMonthPerformance : performancePeriod,
+              performanceLength >= 365 ? product.oneYearPerformance : performancePeriod,
+            ]}
+            wide={false}
+          />
+        )}
+    </div>
+  );
+};
